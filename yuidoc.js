1ec668c45bc95f47493b9c3e9ea2f22a18a94223
Loz Jackson <lozjackson@talktalk.net>
2015-10-26 21:41:52 +0000

    add yuidoc.js

---
+++ yuidoc.js
@@ -0,0 +1,12 @@
+{
+  "name": "ember-clock-face",
+  "description": "A clock face for Ember",
+  "url": "https://github.com/lozjackson/ember-clock-face",
+  "options": {
+    "paths": [
+      "app"
+    ],
+    "exclude": "vendor",
+    "outdir":   "docs"
+  }
+}
