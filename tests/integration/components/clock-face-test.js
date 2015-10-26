7f71bdce4ac33991fd06d7b2cc3a07525d2d78f8
Loz Jackson <lozjackson@talktalk.net>
2015-10-26 13:03:57 +0000

    update clock-face test

--- tests/integration/components/clock-face-test.js
+++ tests/integration/components/clock-face-test.js
@@ -1,28 +1,31 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import ClockService from 'ember-clock/services/clock';
+// import ClockService from 'ember-clock/services/clock';
+// import Ember from 'ember';
 
 moduleForComponent('clock-face', 'Integration | Component | clock face', {
   integration: true,
-  setup: function(container) {
-    container.register('service:clock', ClockService);
-    container.injection('component', 'clock', 'service:clock');
-  }
+  // setup: function( container ) {
+  //   //var app = arguments[1] || arguments[0];
+  //   // Ember.Logger.log('app', container);
+  //   // app.register('service:clock', ClockService);
+  //   // app.inject('component', 'clock', 'service:clock');
+  // }
 });
 
-// test('it renders', function(assert) {
-//  assert.expect(3);
-  
-//  // Set any properties with this.set('myProperty', 'value');
-//   // Handle any actions with this.on('myAction', function(val) { ... });
-//
-//   this.render(hbs`{{clock-face size=clockSize}}`);
-//
-//   assert.equal(this.$().text().trim(), '');
-//
-//   // check the class names
-//   assert.ok($('.ember-clock-face.clock-face').get(0));
-//
-//   // check the tag name
-//   assert.equal(this.$('.ember-clock-face.clock-face').get(0).tagName, 'svg');
-// });
+test('it renders', function(assert) {
+  assert.expect(3);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{clock-face size=clockSize}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // check the class names
+  assert.ok(this.$('.ember-clock-face.clock-face').get(0));
+
+  // check the tag name
+  assert.equal(this.$('.ember-clock-face.clock-face').get(0).tagName, 'svg');
+});
