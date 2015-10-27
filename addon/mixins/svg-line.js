3c44dd27e6d709e2e688bc28ab906eff9433bb88
Loz Jackson <lozjackson@talktalk.net>
2015-10-27 21:37:17 +0000

    add svn-line mixin

---
+++ addon/mixins/svg-line.js
@@ -0,0 +1,78 @@
+/**
+  @module ember-clock-face
+*/
+import Ember from 'ember';
+
+/**
+  @class SvgLineMixin
+  @namespace EmberClockFace
+*/
+export default Ember.Mixin.create({
+
+  /**
+    @property tagName
+    @type {String}
+    @default `line`
+    @private
+  */
+  tagName: 'line',
+
+  /**
+    @property attributeBindings
+    @type {Array}
+    @default `[ 'x1', 'x2', 'y1', 'y2', 'transform' ]`
+    @private
+  */
+  attributeBindings: [ 'x1', 'x2', 'y1', 'y2', 'transform' ],
+
+  /**
+    @property x1
+    @type {Number}
+    @default 50
+    @private
+  */
+  x1: 50,
+
+  /**
+    @property y1
+    @type {Number}
+    @default 50
+    @private
+  */
+  y1: 50,
+
+  /**
+    @property x2
+    @type {Number}
+    @default 50
+    @private
+  */
+  x2: 50,
+
+  /**
+    @property y2
+    @type {Number}
+    @default 0
+    @private
+  */
+  y2:0,
+
+  /**
+    @property rotate
+    @type {Number}
+    @default `0`
+    @private
+  */
+  rotate: 0,
+
+  /**
+    @property transform
+    @type {String}
+    @default `rotate(0 50 50)`
+    @private
+  */
+  transform: Ember.computed('rotate', function() {
+    var rotate = this.get('rotate') || 0;
+    return `rotate(${rotate} 50 50)`;
+  })
+});
---
+++ tests/unit/mixins/svg-line-test.js
@@ -0,0 +1,81 @@
+import Ember from 'ember';
+// import SvgLineMixin from '../../../mixins/svg-line';
+import SvgLineMixin from 'ember-clock-face/mixins/svg-line';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | svg line');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var SvgLineObject = Ember.Object.extend(SvgLineMixin);
+  var subject = SvgLineObject.create();
+  assert.ok(subject);
+});
+
+test('tagName is line', function(assert) {
+  var SvgLineObject = Ember.Object.extend(SvgLineMixin);
+  var subject = SvgLineObject.create();
+
+
+  assert.equal(subject.get('tagName'), 'line', '`tagName` should be `LINE`');
+});
+
+test('test x1', function(assert) {
+  var SvgLineObject = Ember.Object.extend(SvgLineMixin);
+  var subject = SvgLineObject.create();
+
+
+  assert.equal(subject.get('x1'), 50, '`x1` should be `50`');
+
+  subject.set('x1', 25);
+
+  assert.equal(subject.get('x1'), 25, '`x1` should be `25`');
+});
+
+test('test x2', function(assert) {
+  var SvgLineObject = Ember.Object.extend(SvgLineMixin);
+  var subject = SvgLineObject.create();
+
+
+  assert.equal(subject.get('x2'), 50, '`x2` should be `50`');
+
+  subject.set('x2', 25);
+
+  assert.equal(subject.get('x2'), 25, '`x2` should be `25`');
+});
+
+test('test y1', function(assert) {
+  var SvgLineObject = Ember.Object.extend(SvgLineMixin);
+  var subject = SvgLineObject.create();
+
+
+  assert.equal(subject.get('y1'), 50, '`y1` should be `50`');
+
+  subject.set('y1', 25);
+
+  assert.equal(subject.get('y1'), 25, '`y1` should be `25`');
+});
+
+test('test y2', function(assert) {
+  var SvgLineObject = Ember.Object.extend(SvgLineMixin);
+  var subject = SvgLineObject.create();
+
+
+  assert.equal(subject.get('y2'), 0, '`y2` should be `0`');
+
+  subject.set('y2', 25);
+
+  assert.equal(subject.get('y2'), 25, '`y2` should be `25`');
+});
+
+test('test transform', function(assert) {
+  var SvgLineObject = Ember.Object.extend(SvgLineMixin);
+  var subject = SvgLineObject.create();
+
+
+  assert.equal(subject.get('transform'), 'rotate(0 50 50)', '`transform` should be `rotate(0 50 50)`');
+
+  subject.set('rotate', 25);
+
+  assert.equal(subject.get('transform'), 'rotate(25 50 50)', '`transform` should be `rotate(25 50 50)`');
+});
